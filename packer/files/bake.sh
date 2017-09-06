9e47c8e31ff970417114f85ec4cc1579d7a7b9cf
Maksim Anfilatov <maksim@woyager.ru>
2017-09-06 15:30:55 +0300

    add puma-service autostart

--- packer/files/bake.sh
+++ packer/files/bake.sh
@@ -4,3 +4,8 @@ cd /tmp; git clone https://github.com/woyager/infra.git --branch config-scripts
 /tmp/infra/install_ruby.sh
 /tmp/infra/install_mongodb.sh
 /tmp/infra/deploy.sh
+wget https://raw.githubusercontent.com/woyager/infra/base-os-packer/packer/files/puma.service
+cp puma.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable puma-service
+systemctl status puma-service
