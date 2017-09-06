8a02c4587650ada164237cfc487d2d92ec7b75e8
Maksim Anfilatov <maksim@woyager.ru>
2017-09-06 15:27:14 +0300

    add puma.service file and fixes in other scripts

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -2,16 +2,16 @@
 
 # Install MongoDB
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+apt-get update
+apt-get install -y mongodb-org
 
 # Start/enable
 
-sudo systemctl start mongod
-sudo systemctl enable mongod
+systemctl start mongod
+systemctl enable mongod
 
 # Check
 
-sudo systemctl status mongod
+systemctl status mongod
