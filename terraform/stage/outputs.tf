f512c762c5416f93f900587d1c29feb1f4ccaff6
Maksim Anfilatov <maksim@woyager.ru>
2017-09-20 15:19:45 +0300

    Homework 11

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -5,3 +5,12 @@ output "app_external_ip" {
 output "db_external_ip" {
   value = "${module.db.db_external_ip}"
 }
+
+output "app_internal_ip" {
+  value = "${module.app.app_internal_ip}"
+}
+
+output "db_internal_ip" {
+  value = "${module.db.db_internal_ip}"
+}
+
